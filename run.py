1e03b80f64d4f7b19d1443b9d24c2d0b31dcd17e
Novi Amalia Ardha <33639392+noviamaliardha@users.noreply.github.com>
2019-09-05 14:03:49 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,8 +2,9 @@
 import os
 from src.app import create_app
 app = create_app(env_name)
+env_name  =   os.environ['FLASK_ENV']
+
 if __name__ == '__main__':
-  env_name  =   os.environ['FLASK_ENV']
   
   # run app
   app.run()
